a416fecbe1250068938866dad7981ec6c36dc3da
Sipang K <plaaom39@gmail.com>
2023-08-22 22:14:15 +0700

    chore: change field name from 'id' to 'user_id'

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -1,13 +1,14 @@
-import express, { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { pool } from "../config";
 import { RowDataPacket, ResultSetHeader } from "mysql2";
 
 export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const [users] = await pool.query<RowDataPacket[]>("SELECT * FROM users;");
-    if (users.length > 0) {
-      return res.status(200).json({ users }).end();
+    if (users.length === 0) {
+      throw new Error("No users found");
     }
+    return res.status(200).json({ users }).end();
   } catch (error: any) {
     next(error);
     return res.status(400).json({ status: "error", message: error.message });
@@ -21,6 +22,9 @@ export const getUserByEmail = async (req: Request, res: Response, next: NextFunc
       throw new Error("Please enter a valid email address");
     }
     const [user] = await pool.query<RowDataPacket[]>("SELECT * FROM `users` WHERE email = ?;", [email]);
+    if (user.length === 0) {
+      throw new Error("No users found");
+    }
     return res.status(200).json({ user }).end();
   } catch (error: any) {
     next(error);
@@ -39,13 +43,10 @@ export const updateUserByID = async (req: Request, res: Response, next: NextFunc
       };
     }
     // Update user
-    const [result] = await pool.query<ResultSetHeader>("UPDATE users SET `f_name`=?, `l_name`=?, `email`=?, `password`=? WHERE `id` = ?", [
-      f_name,
-      l_name,
-      email,
-      password,
-      id,
-    ]);
+    const [result] = await pool.query<ResultSetHeader>(
+      "UPDATE users SET `f_name`=?, `l_name`=?, `email`=?, `password`=? WHERE `user_id` = ?",
+      [f_name, l_name, email, password, id]
+    );
 
     // Not found user
     if (result.affectedRows == 0) {
@@ -70,7 +71,7 @@ export const updateUserByID = async (req: Request, res: Response, next: NextFunc
 export const deleteUserByID = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
-    const [result] = await pool.query<ResultSetHeader>("DELETE FROM users WHERE id = ?;", [id]);
+    const [result] = await pool.query<ResultSetHeader>("DELETE FROM users WHERE user_id = ?;", [id]);
     // Not found user
     if (result.affectedRows == 0) {
       throw {
