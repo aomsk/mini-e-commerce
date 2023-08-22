2e7b3e9952f34fb69103446ed7f5597a5a5fba86
Sipang K <plaaom39@gmail.com>
2023-08-22 13:13:32 +0700

    chore: update user route

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -1,108 +1,84 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { pool } from "../config";
 import { RowDataPacket, ResultSetHeader } from "mysql2";
 
-export const getAllUsers = (req: express.Request, res: express.Response) => {
+export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const sql = "SELECT * FROM `users`;";
-    pool.query<RowDataPacket[]>(sql, (_err, rows) => {
-      if (_err) {
-        return res.status(400).json({ status: "error", message: _err?.message });
-      }
-      if (rows.length > 0) {
-        return res.status(200).json({ users: rows }).end();
-      }
-    });
+    const [users] = await pool.query<RowDataPacket[]>("SELECT * FROM users;");
+    if (users.length > 0) {
+      return res.status(200).json({ users }).end();
+    }
   } catch (error: any) {
+    next(error);
     return res.status(400).json({ status: "error", message: error.message });
   }
 };
 
-export const getUserByEmail = (req: express.Request, res: express.Response) => {
+export const getUserByEmail = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email } = req.body;
-    const sql = "SELECT * FROM `users` WHERE email = ?;";
-    pool.query<RowDataPacket[]>(sql, [email], (_err, rows) => {
-      if (_err) {
-        return res.status(400).json({ status: "error", message: _err?.message });
-      }
-      if (rows.length == 0) {
-        return res.status(404).json({ status: "not found", message: "User not found" }).end();
-      }
-      if (rows.length == 1) {
-        return res.status(200).json({ users: rows }).end();
-      }
-    });
+    if (!email) {
+      throw new Error("Please enter a valid email address");
+    }
+    const [user] = await pool.query<RowDataPacket[]>("SELECT * FROM `users` WHERE email = ?;", [email]);
+    return res.status(200).json({ user }).end();
   } catch (error: any) {
+    next(error);
     return res.status(400).json({ status: "error", message: error.message });
   }
 };
 
-export const createUser = (req: express.Request, res: express.Response) => {
-  try {
-    const { f_name, l_name, email, password } = req.body;
-    const sql = "INSERT INTO `users` (f_name, l_name, email, password) VALUES (?, ?, ?, ?);";
-    pool.query<ResultSetHeader>(sql, [f_name, l_name, email, password], (_err, result) => {
-      if (_err) {
-        return res.status(400).json({ status: "error", message: _err?.message });
-      }
-      if (result.affectedRows == 1) {
-        const user = {
-          id: result.insertId,
-          f_name,
-          l_name,
-          email,
-          password,
-        };
-        return res.status(201).json({ message: "create user successfully", user }).end();
-      }
-    });
-  } catch (error: any) {
-    return res.status(400).json({ status: "error", message: error.message });
-  }
-};
-
-export const updateUserByID = (req: express.Request, res: express.Response) => {
+export const updateUserByID = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
     const { f_name, l_name, email, password } = req.body;
     if (!f_name || !l_name || !email || !password) {
-      return res.status(400).json({ status: "error", message: "Missing some feild" });
+      throw {
+        message: "Please enter a first name, last name, email, or password",
+        httpStatusCode: 400,
+      };
     }
-    const sql = "UPDATE `users` SET `f_name`=?, `l_name`=?, `email`=?, `password`=? WHERE `id` = ?";
-    pool.query<ResultSetHeader>(sql, [f_name, l_name, email, password, id], (_err, result) => {
-      if (_err) {
-        return res.status(400).json({ status: "error", message: _err?.message });
-      }
-      if (result.affectedRows == 1 && result.changedRows == 1) {
-        const user = {
-          id,
-          f_name,
-          l_name,
-          email,
-          password,
-        };
-        return res.status(200).json({ message: "update user successfully", user }).end();
-      }
-      return res.status(200).json({ message: "user data not change" }).end();
-    });
+    // Update user
+    const [result] = await pool.query<ResultSetHeader>("UPDATE users SET `f_name`=?, `l_name`=?, `email`=?, `password`=? WHERE `id` = ?", [
+      f_name,
+      l_name,
+      email,
+      password,
+      id,
+    ]);
+
+    // Not found user
+    if (result.affectedRows == 0) {
+      throw {
+        message: "Cannot update users because user not found",
+        httpStatusCode: 400,
+      };
+    }
+    const user = {
+      id,
+      f_name,
+      l_name,
+      email,
+      password,
+    };
+    return res.status(200).json({ message: "Update user successfully", user }).end();
   } catch (error: any) {
-    return res.status(400).json({ status: "error", message: error.message });
+    return res.status(error.httpStatusCode).json({ status: "error", message: error.message });
   }
 };
 
-export const deleteUserByID = (req: express.Request, res: express.Response) => {
+export const deleteUserByID = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
-    const sql = "DELETE FROM users WHERE id = ?";
-    pool.query<ResultSetHeader>(sql, [id], (_err, result) => {
-      if (_err) {
-        return res.status(400).json({ status: "error", message: _err?.message });
-      }
-      if (result.affectedRows == 1) {
-        return res.status(200).json({ message: "delete user successfully" }).end();
-      }
-    });
+    const [result] = await pool.query<ResultSetHeader>("DELETE FROM users WHERE id = ?;", [id]);
+    // Not found user
+    if (result.affectedRows == 0) {
+      throw {
+        message: "Cannot delete users because user not found",
+        httpStatusCode: 400,
+      };
+    }
+    return res.status(200).json({ message: "delete user successfully" }).end();
   } catch (error: any) {
     return res.status(400).json({ status: "error", message: error.message });
   }
--- server/src/routes/users.route.ts
+++ server/src/routes/users.route.ts
@@ -1,17 +1,10 @@
 import express from "express";
-import {
-  getAllUsers,
-  getUserByEmail,
-  createUser,
-  updateUserByID,
-  deleteUserByID,
-} from "../controllers/users.controller";
+import { getAllUsers, getUserByEmail, updateUserByID, deleteUserByID } from "../controllers/users.controller";
 
 const userRouter = express.Router();
 
 userRouter.get("/api/users", getAllUsers); // GET all users
 userRouter.get("/api/user", getUserByEmail); // GET user by email
-userRouter.post("/api/user", createUser); // POST create new user
 userRouter.put("/api/user/:id", updateUserByID); // PUT update user by id
 userRouter.delete("/api/user/:id", deleteUserByID); // DELETE delete user by id
 
