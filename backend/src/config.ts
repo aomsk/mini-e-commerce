5b5237d6f963eb3e545b7237b9e0fab475794885
Sipang K <plaaom39@gmail.com>
2023-09-07 10:56:39 +0700

    build: update config database file

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,13 +1,14 @@
 import mysql, { PoolOptions } from "mysql2/promise";
 
 const access: PoolOptions = {
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "",
-  database: process.env.MYSQL_DATABASE || "e-commerce",
-  host: process.env.MYSQL_HOST || "localhost",
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  port: Number(process.env.MYSQL_PORT),
 };
 
 export const pool = mysql.createPool(access);
